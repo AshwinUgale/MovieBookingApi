ab0ce7032787779cd85fa5ecb91d3c1840c8072d
Ashwin Govind Ugale <ugaleashwin@gmail.com>
2025-03-22 17:17:54 -0400

    fakeShowtimesSeatGridAndBookings

--- .history/index_20250322171237.js
+++ .history/index_20250322171237.js
@@ -3,11 +3,15 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const errorHandler = require('./middleware/errorHandler');
 const connectDB = require('./config/db');
+const listEndpoints = require("express-list-endpoints");
+
 
 const authRoutes = require('./routes/authRoutes');
 const movieRoutes = require('./routes/movieRoutes');
 const showtimeRoutes = require('./routes/showtimeRoutes');
-
+const bookingRoutes = require('./routes/bookingRoutes');
+const paymentRoutes = require('./routes/paymentRoutes');
+const ticketmasterRoutes = require("./routes/ticketmasterRoutes");
 dotenv.config();
 
 
@@ -26,6 +30,9 @@ app.use(errorHandler);
 app.use('/api/auth', authRoutes);
 app.use('/api/movies', movieRoutes);
 app.use('/api/showtimes',showtimeRoutes);
+app.use('/api/bookings', bookingRoutes); 
+app.use('/api/payments', paymentRoutes);
+app.use("/api/ticketmaster", ticketmasterRoutes);
 
 app.use((req, res, next) => {
     res.status(404).json({ message: 'Route not found' });
@@ -36,5 +43,9 @@ app.use(errorHandler);
 
 
 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=> console.log(`Server running on port ${PORT}`));
+console.log("📌 Registered Routes:");
+console.table(listEndpoints(app));
+
